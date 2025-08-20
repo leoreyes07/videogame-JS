a0eeb1f45c27753bef321ad7ad60a7036f4d9260
Leo R <leoreyes_07@outlook.com>
2025-08-19 21:54:51 -0600

    added the skull icon and added movement around the canvas

--- games.js
+++ games.js
@@ -1,75 +1,103 @@
-const canvas = document.querySelector('#game');
-const game = canvas.getContext('2d');
-const btnUp = document.querySelector('#up')
-const btnRight = document.querySelector('#right')
-const btnLeft = document.querySelector('#left')
-const btnDown = document.querySelector('#down')
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+const btnUp = document.querySelector("#up");
+const btnRight = document.querySelector("#right");
+const btnLeft = document.querySelector("#left");
+const btnDown = document.querySelector("#down");
 
 let canvasSize;
 let elementsSize;
 
-window.addEventListener('load', setCanvasSize);
-window.addEventListener('resize', setCanvasSize);
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
+
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 
 function setCanvasSize() {
-    if (window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.8;
-    } else {
-       canvasSize = window.innerHeight * 0.8;
-    }
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
 
-    canvas.setAttribute('width', canvasSize)
-    canvas.setAttribute('height', canvasSize)
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
 
-    elementsSize = (canvasSize / 10) - 2
+  elementsSize = canvasSize / 10 - 1;
 
-    startGame();
+  startGame();
 }
 
-function startGame () {
-    
-    console.log({ canvasSize, elementsSize });
+function startGame() {
+  console.log({ canvasSize, elementsSize });
 
-    game.font = elementsSize + 'px Verdana';
-    game.textAlign = '';
-    
-    const map = maps[0];
-    const mapRows = map.trim().split('\n');
-    const mapCols = mapRows.map(row => row.trim().split(''));
-    console.log({map, mapRows, mapCols});
-    
-    mapCols.forEach((row, rowI) => {
-        row.forEach((col, colI) => {
-            const emoji = emojis[col];
-            const posX = elementsSize * (colI + 1);
-            const posY = elementsSize * (rowI + 1);
-            game.fillText(emoji, posX, posY);
-        })
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "";
+
+  const map = maps[0];
+  const mapRows = map.trim().split("\n");
+  const mapCols = mapRows.map((row) => row.trim().split(""));
+  console.log({ map, mapRows, mapCols });
+
+  game.clearRect(0, 0, canvasSize, canvasSize);
+  mapCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementsSize * (colI + 0.1);
+      const posY = elementsSize * (rowI + 1);
+
+      if (col == "O") {
+        if (!playerPosition.x && !playerPosition.y) {
+          playerPosition.x = posX;
+          playerPosition.y = posY;
+          console.log({ playerPosition });
+        }
+      }
+
+      game.fillText(emoji, posX, posY);
     });
+  });
+
+  movePlayer();
 }
 
-window.addEventListener('keydown', moveByKeys);
-btnUp.addEventListener('click', moveUp);
-btnRight.addEventListener('click', moveRight);
-btnLeft.addEventListener('click', moveLeft);
-btnDown.addEventListener('click', moveDown);
+function movePlayer() {
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+window.addEventListener("keydown", moveByKeys);
+btnUp.addEventListener("click", moveUp);
+btnRight.addEventListener("click", moveRight);
+btnLeft.addEventListener("click", moveLeft);
+btnDown.addEventListener("click", moveDown);
 
 function moveByKeys(event) {
-    if (event.key == 'ArrowUp') moveUp();
-    else if (event.key == 'ArrowRight') moveRight();
-    else if (event.key == 'ArrowLeft') moveLeft();
-    else if (event.key == 'ArrowDown') moveDown();
-    }
+  if (event.key == "ArrowUp") moveUp();
+  else if (event.key == "ArrowRight") moveRight();
+  else if (event.key == "ArrowLeft") moveLeft();
+  else if (event.key == "ArrowDown") moveDown();
+}
 
 function moveUp() {
-    console.log('Me quiero mover hacia arriba');    
+  console.log("Me quiero mover hacia arriba");
+  playerPosition.y -= elementsSize;
+  startGame();
 }
 function moveRight() {
-    console.log('Me quiero mover hacia la derecha');    
+  console.log("Me quiero mover hacia la derecha");
+  playerPosition.x += elementsSize;
+  startGame();
 }
 function moveLeft() {
-    console.log('Me quiero mover hacia la izquierda');    
+  console.log("Me quiero mover hacia la izquierda");
+  playerPosition.x -= elementsSize;
+  startGame();
 }
 function moveDown() {
-    console.log('Me quiero mover hacia abajo');    
+  console.log("Me quiero mover hacia abajo");
+  playerPosition.y += elementsSize;
+  startGame();
 }
